70e4e552ea3c70f6e5e1b9ce0bf32ecd3f564302
pradip <pradip@gmsil.com>
2024-11-28 17:21:44 +0530

    second commit

--- Frontend/src/router/SignUp.jsx
+++ Frontend/src/router/SignUp.jsx
@@ -18,7 +18,7 @@ const navigate = useNavigate()
       toast.error('invalid email',{className:'toast'})
       return
      }
-await axios.post('https://moviefind-2.onrender.com/movie/sign',{username,email,password},{withCredentials:true})
+await axios.post('https://moviefind-2.onrender.com/movie/sign',{username,email,password})
   .then((response)=>{
     console.log(response)
     if(response.request.status == 200){
