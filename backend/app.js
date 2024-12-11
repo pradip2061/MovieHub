26a20bac8ff59833157b449dc43a38bc1f281837
pradip <pradip@gmsil.com>
2024-12-11 14:35:30 +0530

    message

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ connectToDatabase();
 
 app.use('/movie', userRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`The project is running at port ${PORT}`);
