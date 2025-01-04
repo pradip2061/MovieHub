c64cf51d57e0205d8476d7041159882adc88c997
pradip <pradip@gmsil.com>
2025-01-04 22:50:32 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 app.use(express.static('./storage'));
 
 app.use(cors({
-    origin: ['http://localhost:5173','https://moviefind-five.vercel.app/ '],
+    origin: ['https://moviefind-five.vercel.app/ '],
     credentials: true
 }));
 
